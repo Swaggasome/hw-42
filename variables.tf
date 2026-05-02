e25ec91e98b87b0180efabb4f5c202a81d905034
Swaggasome <4galkin@gmail.com>
2026-05-02 15:07:09 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,9 @@ variable "cloud_id" {
   description = "Yandex Cloud ID"
   type        = string
 }
+
+variable "ssh_public_key" {
+  description = "Public SSH key content"
+  type        = string
+  sensitive   = true
+}
