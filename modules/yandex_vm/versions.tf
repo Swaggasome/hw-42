ca1f18568981495941d773cda0558c3898b0960e
Галкин Владимир <4galkin@gmail.com>
2026-05-02 16:17:44 +0300

    terraform fmt

--- modules/yandex_vm/versions.tf
+++ modules/yandex_vm/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = ">= 0.200.0"
+      version = "0.200.0"
     }
   }
   required_version = ">= 0.13"
