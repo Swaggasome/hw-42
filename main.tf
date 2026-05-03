79822b257120703b9584753472ebccea89794051
Галкин Владимир <4galkin@gmail.com>
2026-05-03 08:58:45 +0300

    Fix

--- main.tf
+++ main.tf
@@ -49,12 +49,12 @@ resource "yandex_vpc_security_group" "web_sg" {
 
 # 3. Используем наш модуль для создания ВМ
 module "my_first_vm" {
-  source         = "./modules/yandex_vm"
-  vm_name        = "web-server-1"
-  subnet_id      = yandex_vpc_subnet.default.id
-  cores          = 2
-  memory         = 2
-  depends_on     = [yandex_vpc_security_group.web_sg]
-  ssh_public_key = var.ssh_public_key
+  source          = "./modules/yandex_vm"
+  vm_name         = "web-server-1"
+  subnet_id       = yandex_vpc_subnet.default.id
+  cores           = 2
+  memory          = 2
+  depends_on      = [yandex_vpc_security_group.web_sg]
+  ssh_public_key  = var.ssh_public_key
   ssh_private_key = var.ssh_private_key
 }
